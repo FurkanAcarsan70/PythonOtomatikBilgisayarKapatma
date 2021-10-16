4251a8f237a40e621dcf636234b2b29a8f55342b
Furkan <furkanacarsanpro@gmail.com>
2021-10-16 15:46:04 +0300

    Kurulum dosyasi

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Bilgisayarı Otomatik Kapatma Programı',
+    version='0.0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='Furkan',
+    author_email='furkanacarsanpro@gmail.com',
+    description='Otomatik Bilgisayar Kapatma'
+)
